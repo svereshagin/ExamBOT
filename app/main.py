1bfc8d4c37e1a7ccf585776e83d8813520917d39
amadeo <risefenixpuryfire@gmail.com>
2025-02-26 00:56:44 +0300

    new_module_to_be_added(yml, links, dependencies)

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
+import yaml
+from app.src.selenium_parcer.parcer import run_registration
 
+def get_links() -> list:
+    with open('app/links.yml', 'r', encoding='UTF-8') as file:
+        data = yaml.safe_load(file)
+    return data['links']
 
-links = [
-    'https://portal.mfua.ru/journal_new/?curSeason=Autumn&curDiscipline=64ad49f6-a933-11de-87a2-00145e6e635e&curGroup=b0bf7328-d8fd-11ed-80ec-000c294da1bd',
-    'https://portal.mfua.ru/journal_new/?curSeason=Autumn&curDiscipline=64ad49f6-a933-11de-87a2-00145e6e635e&curGroup=74d7b4f1-4637-11ee-80f2-000c294da1bd'
-]
 from app.src.selenium_parcer.parcer import run_registration
 
 if '__main__' == __name__:
+    links = get_links()
     res = run_registration(links)
     print(res)
 
